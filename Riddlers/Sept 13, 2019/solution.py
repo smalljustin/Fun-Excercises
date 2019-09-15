7e6a11fd6ac49cd8c2b12d3a9ee0ab1bddd7837a
Justin Schmitz <jschmitz2@hawk.iit.edu>
2019-09-14 21:55:54 -0500

    Solved problem
    Solved problem using what can be best described as an adaptation of DFS, brute forcing all possible combinations of routes. Best length: 23, with string GAKCAHIALARIDCOKSCTNASD. See champ_paths.csv for all the 'champ' paths as discovered.

--- Riddlers/Sept 13, 2019/solution.py
+++ Riddlers/Sept 13, 2019/solution.py
@@ -1,3 +1,6 @@
+import sys
+sys.setrecursionlimit(150)
+
 states = {
         'AK': 'Alaska',
         'AL': 'Alabama',
@@ -58,40 +61,142 @@ states = {
         'WY': 'Wyoming'
 }
 
+
 class Connection:
-    def __init__(self, from, to):
-        self.from = from
-        self.to = to
+    def __init__(self, nodes, end):
+        self.end = end
         self.used = False
 
 class Node:
-    def __init__(self, abbv):
-        self.name = abbv[0]
+    def __init__(self, name):
+        self.name = name
         self.routes = []
 
-    def add_route(to):
-        self.routes.append(Connection(self.name, to))
+    def add_route(self, nodes, end):
+        self.routes.append(Connection(nodes, end))
 
-    def f(self):
-        return self.name[0]
+    def route(self, route):
+        routes = []  # List of Connection objects
+        for route in self.routes:
+            if not route.used:
+                route.used = True
+                routes.append(route.end.route())
+        else:
+            return None
 
-    def l(self):
-        return self.name[1]
+    def navigate(self, past_paths, cur_path):
+        # print(self.name)
+        def path_test(end):
+            for path in past_paths:
+                if path[0:len(cur_path)+1] == cur_path + [end]:
+                    return False
+            else:
+                # print("##################")
+                return True
+        for route in self.routes:
+            if not route.used and path_test(route.end):
+                route.used = True
+                cur_path.append(route.end)
+                # print(f"Navigating from {self.name} to {route.end.name}")
+                route.end.navigate(past_paths, cur_path)
+                return cur_path
+        else:
+            # self.print_route(cur_path)
+            return cur_path
 
-    def __repr__(self):
-        return self.name
+    def reset_routes(self):
+        for route in self.routes:
+            if route.used:
+                route.used = False
+                route.end.reset_routes()
+
+    @staticmethod
+    def print_route(route):
+        outstring = ""
+        for node in route:
+            outstring += node.name
+        print(outstring)
+
+    @staticmethod
+    def string_route(route):
+        outstring = ""
+        for node in route:
+            outstring += node.name
+        return outstring
 
 nodes = {}
+
+def set_connections(route):
+    for i in range(len(route)):
+        node = route[i]
+        for conn in node.routes:
+            if i > len(route)-2:
+                break
+            else:
+                # print(conn.end == route[i+1])
+                if conn.end == route[i+1]:
+                    conn.used = True
+                    # print("Connection set.")
+                    break
+
 for key in states.keys():
-    if key[0] in states.keys():
+    if key[0] in nodes.keys():
         pass
     else:
         nodes[key[0]] = Node(key[0])
 
 for key in states.keys():
-    for node in nodes.values():
-        if key[1] in nodes.keys():
-            nodes[key[0]].routes[key[0]] = nodes[key[1]]
+    node = nodes[key[0]]
+    if key[1] in nodes:
+        node.add_route(nodes, nodes[key[1]])
 
+# for node in nodes.values():
+#     for route in node.routes:
+#         print(node.name, route.end.name)
+
+# Method: Using DFS, and avoiding past routes:
+
+past_paths = []
 for node in nodes.values():
-    print(node.routes)
+    cur_path = [node]
+    path = node.navigate(past_paths, cur_path)
+    # node.print_route(path)
+    past_paths.append(path)
+    node.reset_routes()
+    for i in range(len(path)):
+        node.reset_routes()
+        set_connections(path[0:i])
+        new_path = path[-1].navigate(past_paths, path[0:i])
+        node.print_route(new_path)
+        past_paths.append(new_path)
+
+    node.reset_routes()
+
+max_len = 0
+max_path = []
+total = 0
+
+champ_paths = open("champ paths.csv", "w+")
+
+for path in past_paths:
+    if len(path) > 0:
+        node = path[0]
+        for i in range(len(path)):
+            node.reset_routes()
+            set_connections(path[0:i])
+            new_path = path[-1].navigate(past_paths, path[0:i])
+            total += 1
+            if total % 1000 == 0:
+                print(f"Checked {total} routes so far.")
+            if len(new_path) > 22:
+                champ_paths.write(f"{len(new_path)}, {node.string_route(new_path)}, {total}\n")
+            if len(new_path) > max_len:
+                max_len = len(new_path)
+                max_path = new_path
+                print("New champ found!")
+                print("Length: ", max_len)
+                print("Total checked: ", total)
+                champ_paths.write(f"{len(new_path)}, {node.string_route(new_path)}, {total}\n")
+                node.print_route(max_path)
+            # node.print_route(new_path)
+            past_paths.append(new_path)
